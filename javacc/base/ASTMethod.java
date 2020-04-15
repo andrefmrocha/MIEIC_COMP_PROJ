a34461d6126254166786993740fba8d612b6172f
Ana Margarida <anamargaridarl@gmail.com>
2020-04-15 13:03:07 +0100

    refactor

--- javacc/base/ASTMethod.java
+++ javacc/base/ASTMethod.java
@@ -32,40 +32,13 @@ class ASTMethod extends TypeNode {
     }
     //TODO: class must set the table of the method instead of using this
 
-    String method_name = null;
-    Symbol.Type type = null;
-    List<Symbol.Type> parameters_types =  new ArrayList<>();
 
     if (methodType.id == ParserTreeConstants.JJTMETHODNAME){
-      ASTIdentifier namenode = (ASTIdentifier) methodType.jjtGetChild(1);
-      SimpleNode typenode = (SimpleNode) methodType.jjtGetChild(0);
-      method_name = namenode.identifierName;
-      type = VarNode.getType(typenode.id);
-
-      if(parameters != null) {
-        parameters.setTable(this.table);
-        for(int i = 0; i < parameters.jjtGetNumChildren(); i+=2) {
-          VarNode parameter = new VarNode(i,parameters.jjtGetChild(i),parameters.jjtGetChild(i+1),table);
-          parameter.eval();
-          parameters_types.add(parameter.getType(parameters.jjtGetChild(i).getId()));
-        }
-      }
-      this.table.getParent().putSymbol(method_name,new MethodSymbol(type,parameters_types));
+      methodType.setTable(this.table);
+      ((ASTMethodName)methodType).eval(parameters);
     } else if(methodType.id == ParserTreeConstants.JJTMAIN) {
-      if(this.table.getParent().getSymbol("main") != null)
-        throw new SemanticsException("Main is already defined in this class!");
-
-      SimpleNode child = (SimpleNode) methodType.jjtGetChild(0);
-      if(child.id == ParserTreeConstants.JJTIDENTIFIER)
-      {
-        ASTIdentifier identifier = (ASTIdentifier) child;
-        this.table.putSymbol(identifier.identifierName, new Symbol(Symbol.Type.OBJ));
-      }
-      else
-        throw new SemanticsException("Invalid child to body!");
-
-      parameters_types.add(Symbol.Type.OBJ);
-      this.table.getParent().putSymbol("main",new MethodSymbol(Symbol.Type.MAIN,parameters_types));
+      methodType.setTable(this.table);
+      methodType.eval();
     } else {
       throw new SemanticsException("Wrong method type was found");
     }
