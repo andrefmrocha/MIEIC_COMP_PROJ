abd63d4edd82828b64c379413d6eafb5922e288f
João Martins <up201707311@fe.up.pt>
2020-04-22 18:29:17 +0100

    setup of generator writer

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javamm.semantics.SymbolTable;
 import javamm.semantics.MethodSymbolTable;
 import javamm.parser.Parser;
@@ -10,6 +15,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Parser parser;
+		PrintWriter writer;
 		try {
 			parser = new Parser(new java.io.FileInputStream(args[0]));
 			SimpleNode node = parser.Program();
@@ -20,6 +26,16 @@ public class Main {
 			node.setTables(newTable, methodTable);
 			node.eval();
 			node.dump("");
+			Matcher fileMatch = Pattern.compile("(?<=/)?(\\w)+.jmm$").matcher(args[0]);
+			if(!fileMatch.find())
+				throw new IllegalArgumentException();
+			String generated_name = fileMatch.group().replace(".jmm",".j");
+			File file = new File("jasmin_gen/" + generated_name);
+			file.getParentFile().mkdirs();
+			writer = new PrintWriter("jasmin_gen/" + generated_name);
+			writer.println(".source " + generated_name);
+			node.write(writer);
+			writer.close();
 		}catch (ParseException | FileNotFoundException | SemanticsException e){
 			e.printStackTrace();
 			throw new RuntimeException();
