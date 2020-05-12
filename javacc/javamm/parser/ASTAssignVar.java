bf08f83d03d7632671b105023a657a07dbd754c4
João Martins <up201707311@fe.up.pt>
2020-05-12 23:08:11 +0100

    implemented if else code generation

--- javacc/javamm/parser/ASTAssignVar.java
+++ javacc/javamm/parser/ASTAssignVar.java
@@ -9,67 +9,67 @@ import java.io.PrintWriter;
 /* JavaCCOptions:MULTI=true,NODE_USES_Javamm=false,VISITOR=false,TRACK_TOKENS=false,NODE_PREFIX=AST,NODE_EXTENDS=,NODE_FACTORY=,SUPPORT_CLASS_VISIBILITY_PUBLIC=true */
 public
 class ASTAssignVar extends TypeNode {
-    public String varIdentifier;
+  public String varIdentifier;
 
-    public ASTAssignVar(int id) {
-        super(id);
+  public ASTAssignVar(int id) {
+    super(id);
+  }
+
+  public ASTAssignVar(Javamm p, int id) {
+    super(p, id);
+  }
+
+  @Override
+  public void eval(Javamm parser) {
+    if (this.jjtGetNumChildren() != 2) {
+      parser.semanticErrors.add(new SemanticsException("Variable assignment must have two operators", this));
+      return;
     }
 
-    public ASTAssignVar(Javamm p, int id) {
-        super(p, id);
+    SimpleNode identifier = (SimpleNode) this.jjtGetChild(0);
+
+    if (identifier.id != JavammTreeConstants.JJTIDENTIFIER) {
+      parser.semanticErrors.add(new SemanticsException("Variable has not a valid identifier", identifier));
+      return;
     }
 
-    @Override
-    public void eval(Javamm parser) {
-        if (this.jjtGetNumChildren() != 2){
-            parser.semanticErrors.add(new SemanticsException("Variable assignment must have two operators", this));
-            return;
-        }
+    ASTIdentifier temp = (ASTIdentifier) identifier;
+    varIdentifier = temp.identifierName;
 
-        SimpleNode identifier = (SimpleNode) this.jjtGetChild(0);
-
-        if (identifier.id != JavammTreeConstants.JJTIDENTIFIER){
-            parser.semanticErrors.add(new SemanticsException("Variable has not a valid identifier", identifier));
-            return;
-        }
-
-        ASTIdentifier temp = (ASTIdentifier) identifier;
-        varIdentifier = temp.identifierName;
-
-        if (!this.table.checkSymbol(varIdentifier)){
-            parser.semanticErrors.add(new SemanticsException("Variable " + varIdentifier + " does not exist", identifier));
-            return;
-        }
-
-        Symbol symbol = this.table.getSymbol(varIdentifier);
-
-        SimpleNode expression = (SimpleNode) this.jjtGetChild(1);
-        this.evaluateChild(expression, symbol, parser);
-
-        symbol.setInitialized();
+    if (!this.table.checkSymbol(varIdentifier)) {
+      parser.semanticErrors.add(new SemanticsException("Variable " + varIdentifier + " does not exist", identifier));
+      return;
     }
 
-    @Override
-    public void write(PrintWriter writer) {
-        String varName = ((ASTIdentifier) this.jjtGetChild(0)).identifierName;
-        Symbol leftSymbol = this.table.getSymbol(varName);
-        SimpleNode right = (SimpleNode) this.jjtGetChild(1);
+    Symbol symbol = this.table.getSymbol(varIdentifier);
 
-        int varNum = leftSymbol.getStackPos();
-        if(varNum == -1)
-            writer.println("  aload_0");
-        // result will be on stack
-        right.write(writer);
+    SimpleNode expression = (SimpleNode) this.jjtGetChild(1);
+    this.evaluateChild(expression, symbol, parser);
 
-        if(varNum == -1) {
-            String className = this.table.getClassName();
-            String jvmType = Symbol.getJVMTypeByType(leftSymbol.getType());
-            writer.println("  putfield " + className + "/" + varName + " " + jvmType + "\n");
-        } else {
-            String storeInstr = Symbol.getJVMPrefix(leftSymbol.getType()) + "store";
-            String separator = varNum > 3 ? " " : "_";
-            writer.println("  " + storeInstr + separator + Integer.toString(varNum) + "\n");
-        }
+    symbol.setInitialized();
+  }
+
+  @Override
+  public void write(PrintWriter writer) {
+    String varName = ((ASTIdentifier) this.jjtGetChild(0)).identifierName;
+    Symbol leftSymbol = this.table.getSymbol(varName);
+    SimpleNode right = (SimpleNode) this.jjtGetChild(1);
+
+    int varNum = leftSymbol.getStackPos();
+    if (varNum == -1)
+      writer.println("  aload_0");
+    // result will be on stack
+    right.write(writer);
+
+    if (varNum == -1) {
+      String className = this.table.getClassName();
+      String jvmType = Symbol.getJVMTypeByType(leftSymbol.getType());
+      writer.println("  putfield " + className + "/" + varName + " " + jvmType + "\n");
+    } else {
+      String storeInstr = Symbol.getJVMPrefix(leftSymbol.getType()) + "store";
+      String separator = varNum > 3 ? " " : "_";
+      writer.println("  " + storeInstr + separator + Integer.toString(varNum) + "\n");
     }
+  }
 }
 /* JavaCC - OriginalChecksum=661756e145ed220ec46575b3a8adecd3 (do not edit this line) */
---
+++ javacc/javamm/parser/BooleanBinaryOperatorNode.java
@@ -0,0 +1,18 @@
+package javamm.parser;
+
+import javamm.semantics.Symbol;
+
+import java.io.PrintWriter;
+
+public abstract class BooleanBinaryOperatorNode extends BinaryOperatorNode{
+
+  public BooleanBinaryOperatorNode(int i, Symbol.Type type, Symbol.Type op) {
+    super(i, type, op);
+  }
+
+  public BooleanBinaryOperatorNode(Javamm p, int i, Symbol.Type type, Symbol.Type op) {
+    super(p, i, type, op);
+  }
+
+  public abstract void write(PrintWriter writer, String labelFalse);
+}
