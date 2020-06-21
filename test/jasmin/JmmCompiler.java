d49d109123577e9111bb172386cc66d1f6a0f6fd
Tiago Carvalho <tiagodrcarvalho@gmail.com>
2020-06-21 16:49:19 +0000

    JmmCompiler.java edited online with Bitbucket

--- test/jasmin/JmmCompiler.java
+++ test/jasmin/JmmCompiler.java
@@ -1,30 +1,42 @@
 package jasmin;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import pt.up.fe.specs.util.SpecsIo;
+
 import java.io.File;
 import java.lang.reflect.Method;
-import pt.up.fe.specs.util.SpecsIo;
+//import pt.up.fe.specs.util.SpecsIo;
 
 public class JmmCompiler {
 	
-    private static String CLASS_WITH_MAIN = "Main";	
+    private static String CLASS_WITH_MAIN = "JMMCompiler";
 
-	
+
+    /**
+     * Compiles a .jmm file to .j code with no additional arguments.
+     */
+    public static String compile(File jmm){
+        return compile(jmm, new String[0]);
+    }
+
+
 	/**
 	 * Compiles a .jmm file to .j code.
      *
      * @param jmm the .jmm file
      * @return a string with the .j code
 	 */
-	public static String compile(File jmm){
+    public static String compile(File jmm, String... args){
 
 		// This method needs to be implemented before the tests can run
 		// Below there is an example implementation, that assumes that the .j file is generated in the repository root
 		// Adapt the code according to your implementation.
-		//throw new RuntimeException("Implement JmmCompiler.compile() in order to test the execution of .jmm files");
+		// throw new RuntimeException("Implement JmmCompiler.compile() in order to test the execution of .jmm files");
 
 
-
-		// Executes J-- compiler		
 		try {
             // Get class with main
             Class<?> mainClass = Class.forName(CLASS_WITH_MAIN);
@@ -33,25 +45,27 @@ public class JmmCompiler {
             Method mainMethod = mainClass.getMethod("main", String[].class);
 
             // Invoke main method with file as argument
-            String[] mainArgs = { jmm.getAbsolutePath() };
+            List<String> allArgs = new ArrayList<>();
+            allArgs.add(jmm.getAbsolutePath());
+            allArgs.addAll(Arrays.asList(args));
+            String[] mainArgs = allArgs.toArray(size -> new String[size]);
             Object[] invokeArgs = { mainArgs };
             mainMethod.invoke(null, invokeArgs);
 
         } catch (Exception e) {
-			throw new RuntimeException("Error with compiling jmm", e);
+			throw new RuntimeException("Error with compiling jmm: " + e.getMessage(), e);
 		}
 
-		var filename = SpecsIo.removeExtension(jmm.getName()) + ".j";
-		
-		var jFile = new File("jasmin_gen/" + filename);
+		System.out.println(jmm.getAbsolutePath());
+		String filename = SpecsIo.removeExtension(jmm.getAbsolutePath()) + ".j";
+
+		File jFile = new File(filename);
 				
 		if(!jFile.isFile()) {
 			throw new RuntimeException("Could not find file " + jFile.getAbsolutePath());
 		}
 
-		return SpecsIo.read(jFile);		
-
-				
+		return SpecsIo.read(jFile);
 	}
 	
 }
